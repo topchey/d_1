df7f40dd3b258b6558940c51386daeae36fdb251
topcheyalex <topcheyalex@gmail.com>
2021-05-15 06:19:55 +0300

    commit_1

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
         }
     }
 }
