3cb1d2162a78967bf432bfb08c061e05fb8d75bc
topcheyalex <topcheyalex@gmail.com>
2021-05-15 06:20:52 +0300

    Coomit_2

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Git
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World2");
+            Console.WriteLine("Hello World3");
         }
     }
 }
