950b6fd96152602c25589af47681d4ff9a3284ff
topcheyalex <topcheyalex@gmail.com>
2021-05-15 06:35:46 +0300

    commit_4_2

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Git
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World2");
             Console.WriteLine("Hello World3");
+            Console.WriteLine("Hello World4");
         }
     }
 }
